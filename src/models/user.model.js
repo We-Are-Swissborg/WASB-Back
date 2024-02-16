3fee6a16299276e657317d6fb2686d08312c6b3e
Valdior <valdior.tenshi@gmail.com>
2024-02-16 22:38:22 +0100

    init project WASB

---
+++ src/models/user.model.js
@@ -0,0 +1,43 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import seqConnection from '../db/sequelizeConfig.js';
+
+class User extends Model { }
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  pseudo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  walletAdress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  lastLogin: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize: seqConnection,
+  modelName: 'User'
+});
+
+export { User };
