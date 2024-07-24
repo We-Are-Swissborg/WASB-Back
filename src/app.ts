25e2aa0f85439b3494da8d10d60dfb07c27a671b
Valdior <valdior.tenshi@gmail.com>
2024-07-24 09:35:19 +0200

    #27 feat: add options cors and methods

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ const server = createServer(app);
 const corsDomains = process.env.CORS_ORIGIN?.split(',');
 const corsOptions = {
     origin: corsDomains,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     maxAge: 3600,
@@ -26,6 +27,7 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 testConnection();
 if (process.env.NODE_ENV === 'DEV') {
