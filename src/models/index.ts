8837c4d7469d7a7c38af5c1d1584cda2fd94a915
Valdior <valdior.tenshi@gmail.com>
2024-07-26 18:22:39 +0200

    #42 eslint

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ import { logger } from '../middlewares/logger.middleware';
 import { Sequelize } from 'sequelize-typescript';
 
 const env = process.env.NODE_ENV || 'development';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('../../config/config.json')[env];
 
 const sequelize = new Sequelize({
