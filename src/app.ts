48c3ea4a29671b6a8b0ac54b3de8af02f25c1fce
Valdior <valdior.tenshi@gmail.com>
2024-07-30 11:45:15 +0200

    #49 feat * install express-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { logger } from './middlewares/logger.middleware';
 import sequelize from './models';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 /* eslint-disable */
 require('@dotenvx/dotenvx').config();
@@ -13,6 +14,8 @@ require('@dotenvx/dotenvx').config();
 logger.info(`Start application`);
 
 const PORT = process.env.PORT || 3000;
+const limitRequest = process.env.LIMIT_REQUEST || 100;
+const durationLimitRequest = process.env.DURATIONLIMIT_REQUEST || 15;
 const app: Application = express();
 const server = createServer(app);
 const corsDomains = process.env.CORS_ORIGIN?.split(',');
@@ -23,11 +26,16 @@ const corsOptions = {
     credentials: true,
     maxAge: 3600,
 };
+const limiter = rateLimit({
+    windowMs: Number(durationLimitRequest) * 60 * 1000, // 15 minutes
+    max: Number(limitRequest), // limite chaque IP à 100 requêtes par windowMs
+});
 
 sequelize.authenticate().catch(() => logger.error(`Authentication error`));
 
 // Body parsing Middleware
 app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
@@ -39,4 +47,3 @@ app.use('/api', apiRouter);
 server.listen(PORT, () => {
     logger.info(`Listen on port ${PORT}`);
 });
-
