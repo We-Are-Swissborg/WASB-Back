6a6650163dc7c9cb81d020546ef90c70596a265a
Maréchal Pierre <valdior.tenshi@gmail.com>
2024-07-30 16:33:13 +0200

    Panel Admin - Improve security (#50)
    * #49 feat
    * install helmet middleware
    
    * #49 feat
    * install express-rate-limit
    
    * #49 translante comment

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { apiRouter } from './routes/useRoutes';
 import cors from 'cors';
 import { logger } from './middlewares/logger.middleware';
 import sequelize from './models';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 /* eslint-disable */
 require('@dotenvx/dotenvx').config();
@@ -12,6 +14,8 @@ require('@dotenvx/dotenvx').config();
 logger.info(`Start application`);
 
 const PORT = process.env.PORT || 3000;
+const limitRequest = process.env.LIMIT_REQUEST || 100;
+const durationLimitRequest = process.env.DURATIONLIMIT_REQUEST || 15;
 const app: Application = express();
 const server = createServer(app);
 const corsDomains = process.env.CORS_ORIGIN?.split(',');
@@ -22,10 +26,16 @@ const corsOptions = {
     credentials: true,
     maxAge: 3600,
 };
+const limiter = rateLimit({
+    windowMs: Number(durationLimitRequest) * 60 * 1000, // 15 minutes
+    max: Number(limitRequest), // limit IP 100 request/windowMs
+});
 
 sequelize.authenticate().catch(() => logger.error(`Authentication error`));
 
 // Body parsing Middleware
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
@@ -37,35 +47,3 @@ app.use('/api', apiRouter);
 server.listen(PORT, () => {
     logger.info(`Listen on port ${PORT}`);
 });
-
-/******
- * Use only for WebSocket
- ******/
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:5173"]
-//   }
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`connect ${socket.id}`);
-
-//   if (socket.recovered) {
-//     console.log("recovered!");
-//     console.log("socket.rooms:", socket.rooms);
-//     console.log("socket.data:", socket.data);
-//   } else {
-//     console.log("new connection");
-//     socket.join("sample room");
-//     socket.data.foo = "bar";
-//   }
-
-//   socket.on("roucoule", (cb) => {
-//     console.log("ping...");
-//     cb();
-//   });
-
-//   socket.on("disconnect", (reason) => {
-//     console.log(`disconnect ${socket.id} due to ${reason}`);
-//   });
-// });
