da6beae290f6702931829d3c81739ebaf38f187e
Valdior <valdior.tenshi@gmail.com>
2024-09-07 16:04:18 +0200

    #63 feat: delete user

--- src/routes/admin/userAdmin.routes.ts
+++ src/routes/admin/userAdmin.routes.ts
@@ -4,5 +4,7 @@ import * as User from '../../controllers/adminController/userAdmin.controller';
 export const userRouter: Router = express.Router();
 
 userRouter.get('/', User.getAllUsers);
+userRouter.post('/', User.createUser);
 userRouter.get('/:id', User.getUserWithAllInfo);
 userRouter.put('/:id', User.updateUser);
+userRouter.delete('/:id', User.deleteUser);
