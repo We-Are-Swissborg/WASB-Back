5129a12ee7723ceb3ebef0ca44ea0e2ce5486f89
Valdior <valdior.tenshi@gmail.com>
2025-01-13 22:28:22 +0100

    #100 feat: * add models

---
+++ src/models/address.model.ts
@@ -0,0 +1,56 @@
+import { Expose } from "class-transformer";
+import { AutoIncrement, Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+interface IAddress {
+    id: number;
+    street: string;
+    number?: string;
+    postalCode: string;
+    country: string;
+    latitude?: number;
+    longitude?: number;
+    capacity?: number;
+}
+
+@Table
+class Address extends Model implements IAddress {
+    @Expose({ groups: ['all', 'admin'] })
+    @AutoIncrement
+    @PrimaryKey
+    @Column
+    declare id: number;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare street: string;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare number?: string;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare locality?: string;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare postalCode: string;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare country: string;    
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare latitude?: number;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare longitude?: number;
+
+    @Expose({ groups: ['all', 'admin'] })
+    @Column
+    declare capacity?: number;
+}
+
+export { Address, IAddress }
